938fddc13989e91825d12392d1675d9d9ad71681
Ludwin <ludwin.rivera@jalasoft.com>
2018-07-16 12:02:53 -0400

    Initial sharing of project

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public int x;
+
+    public static void Main()
+    {
+        System.out.println("Hola");
+    }
+
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
